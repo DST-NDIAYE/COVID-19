b1d8f428f72b98f7213de707c48d1d28a084e386
Serigne Bassirou NDIAYE <dstndiaye@gmail.com>
2024-07-30 11:24:53 +0200

    Relation Target / Variable

--- git_commit_push.bat
+++ git_commit_push.bat
@@ -1,12 +1,26 @@
 @echo off
+setlocal enabledelayedexpansion
+
 REM Vérifier si un message de commit a été fourni
-if "%*"=="" (
+if "%~1"=="" (
     echo Veuillez fournir un message de commit.
     exit /b 1
 )
 
+REM Construire le message de commit à partir de tous les arguments
+set "commit_message="
+:loop
+if "%~1"=="" goto after_loop
+set "commit_message=!commit_message! %~1"
+shift
+goto loop
+:after_loop
+
+REM Supprimer l'espace initial
+set "commit_message=%commit_message:~1%"
+
 REM Exécuter les commandes Git
 git status
 git add .
-git commit -m "%*"
+git commit -m "%commit_message%"
 git push origin main
